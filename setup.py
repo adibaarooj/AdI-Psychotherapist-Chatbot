ed750a834bc5bfdae8de5a195d64e043689f1383
adibaarooj <adibaarooj@gmail.com>
2025-08-25 02:03:31 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="AdI_bot",
+    version="0.1.0",
+    author="Adiba Arooj",
+    author_email="adibaarooj@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "langchain",
+        "openai",
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "nltk",
+        "spacy",
+        "transformers",
+        "torch",
+        "tensorflow",
+        "flask",
+        "flask-cors",
+        "python-dotenv"
+    ]
+)
